b0d38e341c3e8742380fffc456b5d70b083ccdf2
phatnm3 <phatnm3@fpt.com.vn>
2020-06-01 17:36:21 +0700

    change name model Section

--- src/models/section.js
+++ src/models/section.js
@@ -19,5 +19,13 @@ const sectionSchema = new Schema({
   timestamps: true
 });
 
+sectionSchema.methods.toJSON = function () {
+  const section = this;
+  const sectionObject = section.toObject();
+  delete sectionObject.createdAt;
+  delete sectionObject.updatedAt;
+  return sectionObject;
+}
+
 const Section = new model('Section', sectionSchema);
 module.exports = Section;
